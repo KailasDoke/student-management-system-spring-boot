9d8897a887bf0436020398161ee4ec36f65c5cbb
Kailas <kailasdoke100@gmail.com>
2026-06-02 19:53:30 +0530

    Add project configuration and test setup

---
+++ src/test/java/com/kd/student_management_system/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kd.student_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
